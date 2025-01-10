865d414326becd951c5b50b23c3333a36af75b8a
Sujay <powar.sujay@koptotech.com>
2025-01-10 12:16:41 +0530

    feature all tables creation

---
+++ database/migrations/2025_01_10_055019_create_taxes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('tax_rate');
+            $table->foreignId('tax_group_id')->constrained('tax_groups')->onDelete('cascade');
+            $table->integer('addational_cess');
+            $table->integer('reverse_charge');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taxes');
+    }
+};
