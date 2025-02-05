48b1fd2c2423fe72c162bd414d6b917d1af79f42
Sujay <powar.sujay@koptotech.com>
2025-02-05 19:29:17 +0530

    prerequsite-of-growyourbusiness

--- app/Http/Controllers/Api/GrowYourBusiness.php
+++ app/Http/Controllers/Api/GrowYourBusiness.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Tenant;
 use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 class GrowYourBusiness extends Controller
 {
     public function convertStoreIntoOnlineStore(Request $request){
@@ -19,13 +22,12 @@ class GrowYourBusiness extends Controller
         return response()->json(['message' => 'Stores converted to online stores successfully.']);
     }
 
-   
-   
-   
+
     public function getOnlineProducts(Request $request)
-   {
+{
     $user = Auth::user();
-    $products = Product::where('user_id', $user->id)->where('isonlineproduct',1)
+    $products = Product::where('user_id', $user->id)
+        ->where('isonlineproduct', 1)
         ->with([ 
             'pricing',       
             'wholesalePrice', 
@@ -39,12 +41,30 @@ class GrowYourBusiness extends Controller
     if ($products->isEmpty()) {
         return response()->json(['message' => 'No products found'], 404);
     }
-
-    return response()->json(['products' => $products], 200);
+    $uniqueKey = (string) Str::uuid();
+    return response()->json(['products' => $products,'key' => $uniqueKey], 200);
 }
 
-    public function addToCart(Request $request){
-        dd("Working");
+  
+
+
+
+
+
+public function addToCart(Request $request){
+        $validator = Validator::make($request->all(), [
+            'product_id' => 'required|exists:products,id',
+            'method' => 'nullable',
+            'key' => 'required'
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+        
     }
 
 }
